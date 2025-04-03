73df55311cf2587129bcc668af4f047f868b39c2
Milan Felix Šulc <rkfelix@gmail.com>
2025-04-03 21:50:48 +0200

    Init terraform repos

--- scripts/dump-repos.php
+++ scripts/dump-repos.php
@@ -4,7 +4,8 @@ use Milo\Github\Api;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$pages = [1];
+$cb = fn($fn) => $fn;
+$pages = [1, 2, 3];
 $repos = [];
 
 $api = new Api();
@@ -16,9 +17,18 @@ foreach ($pages as $page) {
 		return [
 			'org' => 'contributte',
 			'name' => $repo['name'],
+			'description' => $repo['description'],
+			'homepage' => $repo['homepage'],
+			'topics' => $repo['topics'],
+			'archived' => $repo['archived'],
+			'has_wiki' => $repo['has_wiki'],
+			'has_discussions' => $repo['has_discussions'],
+			'has_pages' => $repo['has_pages'],
 		];
 	}, $content);
 	$repos = array_merge($repos, $content);
 }
 
 file_put_contents(__DIR__ . '/../data/repos-v1.json', json_encode($repos, JSON_PRETTY_PRINT));
+
+echo "Dumped {$cb(count($repos))} repos.";
